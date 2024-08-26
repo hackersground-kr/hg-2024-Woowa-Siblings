41cec770efee6ff7efde3869ae22833a9d6c1790
윤석규 <greeentea.dev@gmail.com>
2024-08-27 00:59:09 +0900

    오류해결

--- canbus-web/app.js
+++ canbus-web/app.js
@@ -1,34 +1,35 @@
+
 const express = require('express')
-  const path = require('path');
-  const app = express()
+const path = require('path');
+const app = express()
 
-  // 라우트 정의
-  app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'main', 'main.html'));
-  });
+// 라우트 정의
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'main', 'main.html'));
+});
 
-  app.get('/auth/login', (req, res) => {
-    res.sendFile(path.join(__dirname, 'auth', 'signin', 'signin.html'));
-  });
+app.get('/auth/login', (req, res) => {
+  res.sendFile(path.join(__dirname, 'auth', 'signin', 'signin.html'));
+});
 
-  app.get('/auth/register', (req, res) => {
-    res.sendFile(path.join(__dirname, 'auth', 'signup', 'signup.html'));
-  });
+app.get('/auth/register', (req, res) => {
+  res.sendFile(path.join(__dirname, 'auth', 'signup', 'signup.html'));
+});
 
-  app.get('/map', (req, res) => {
-    res.sendFile(path.join(__dirname, 'kakaomap', 'map.html'));
-  });
+app.get('/map', (req, res) => {
+  res.sendFile(path.join(__dirname, 'kakaomap', 'map.html'));
+});
 
-  app.get('/sheet', (req, res) => {
-    res.sendFile(path.join(__dirname, 'sheet', 'sheet.html'));
-  });
+app.get('/sheet', (req, res) => {
+  res.sendFile(path.join(__dirname, 'sheet', 'sheet.html'));
+});
 
-  app.get('/mybus', (req, res) => {
-    res.sendFile(path.join(__dirname, 'busRoute', 'busRouteSheet.html'));
-  });
-  
+app.get('/mybus', (req, res) => {
+  res.sendFile(path.join(__dirname, 'busRoute', 'busRouteSheet.html'));
+});
 
-  // 서버 시작
-  app.listen(3000, () => {
-    console.log('Server running on port 3000');
-  });
+
+// 서버 시작
+app.listen(3000, () => {
+  console.log('Server running on port 3000');
+});
